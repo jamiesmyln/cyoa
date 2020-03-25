2aee4e8b024561857543da2e6bd145b5322f7cfa
Jamie <jamie.smylnycky@cengage.com>
2020-03-25 16:03:59 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function init() {
+  var ch_1_op_1 = "Can this wait until I get dressed?";
+  var ch_1_op_2 = "Come in, Archimedes! We should read it right away.";
+
+  document.getElementById("js-ch_1_op_1").innerHTML = ch_1_op_1;
+  document.getElementById("js-ch_1_op_2").innerHTML = ch_1_op_2;
+}
+
+init();
